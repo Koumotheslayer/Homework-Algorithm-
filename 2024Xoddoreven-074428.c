299c326de559c8914fa474d963078ef0efe741ba
Ouchetati Abdelkarim <kimologicom@gmail.com>
2024-10-27 14:58:26 +0100

    Add files via upload

---
+++ 2024Xoddoreven-074428.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main( ) {
+int X;
+printf("enter the value of X");
+scanf("%d",&X);
+if  (X%2== 0) {
+printf("X is even");
+}
+else{
+printf("X is odd");
+}
+}
+//by Ouchetati Abdelkarim
